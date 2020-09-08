6df76f830aacc8993a9cf4c3641ea3f0a240839d
Kim Daryl Famenial <darylfamenialph@gmail.com>
2020-09-09 06:21:14 +0800

    Add project files.

---
+++ ReflectionApp/Controllers/FileUploadController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReflectionApp.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("FileUpload")]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            var filePath = string.Empty;
+
+            if(file.Length > 0)
+            {
+                var filepath = Path.Combine(Directory.GetCurrentDirectory(), file.FileName);
+                filePath = filepath;
+                using (var stream = new FileStream(filepath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            return RedirectToAction("Detailed", "Home", new { filePath });
+        }
+    }
+}
---
+++ ReflectionApp/Models/AssemblyProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReflectionApp.Models
+{
+    public class AssemblyProperties
+    {
+        public string type { get; set; }
+        public int no_of_methods { get; set; }
+
+        public string method { get; set; }
+    }
+}
